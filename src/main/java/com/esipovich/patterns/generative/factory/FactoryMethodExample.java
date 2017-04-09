cafcc72fe360a671e626ebf55b912da9454bd944
7leonardo7 <esipovich92@gmail.com>
2017-04-09 19:37:45 +0300

    factory strengths and weaknesses

--- src/main/java/com/esipovich/patterns/generative/factory/FactoryMethodExample.java
+++ src/main/java/com/esipovich/patterns/generative/factory/FactoryMethodExample.java
@@ -5,6 +5,11 @@ import com.esipovich.patterns.generative.factory.creators.ConcreteCreatorB;
 import com.esipovich.patterns.generative.factory.creators.Creator;
 import com.esipovich.patterns.generative.factory.products.Product;
 
+/*  Factory
+ *  Определяет интерфейс для создания объектов, но позволяет
+ *  подклассам изменять тип создаваемых объектов
+ */
+
 public class FactoryMethodExample {
     public static void main(String[] args) {
         Creator[] creators = {new ConcreteCreatorA(), new ConcreteCreatorB()};
@@ -14,3 +19,14 @@ public class FactoryMethodExample {
         }
     }
 }
+
+/*  Плюсы
+ *  Реализует принцип открытости/закрытости
+ *  Позволяет не привязывать код приложения к конкретным классам
+ *  Упрощает программу за счёт выноса кода создания объектов в одно место
+ *  Упрощает внесение в программу объектов новых классов
+ */
+
+/*  Минусы
+ *  Для указания типа создаваемых объектов требуются подклассы
+ */
